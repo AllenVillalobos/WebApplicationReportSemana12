99ed2390baf35ee8055a368c592f08ff50d267e2
AllenVillalobos <allenvs06@gmail.com>
2025-07-30 19:36:34 -0600

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationReport
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Persona()
+        {
+        }
+    }
+}
